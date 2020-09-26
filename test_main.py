c3f577c58a1e875cd9a48ec4ac546da439907cb9
Andrei D. Robu <andreidrobu@gmail.com>
2020-09-26 20:00:55 +0100

    add api ok/error response tests

--- test_main.py
+++ test_main.py
@@ -1,6 +1,9 @@
 
+import json
 import unittest.mock
 from unittest.mock import MagicMock
+from bottle import response
+from boddle import boddle
 import main
 
 def test_sum_numbers_to_add():
@@ -15,3 +18,18 @@ def test_memoized_summation_once_only():
         main.memoized_summation()
         main.memoized_summation()
         mock_internal.assert_called_once()
+
+def test_total_ok_response():
+    with boddle():
+        with unittest.mock.patch('main.memoized_summation', lambda: 5):
+            answer = json.loads(main.total())
+        assert answer['total'] == 5
+        assert response.status_code == 200
+
+def test_total_error_response():
+    ''' check that the api returns a 500 response if an error happens '''
+    with boddle():
+        with unittest.mock.patch('main.memoized_summation', lambda: 1 / 0):
+            answer = json.loads(main.total())
+        assert 'error' in answer
+        assert response.status_code == 500
