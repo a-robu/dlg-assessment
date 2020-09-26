59056a9a6a8f98d8063a808449b886b142986126
Andrei D. Robu <andreidrobu@gmail.com>
2020-09-26 19:34:43 +0100

    move comment inside memoized_summation and add docstring

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ from backend import numbers_to_add
 def sum_numbers_to_add(numbers=numbers_to_add):
     return sum(numbers)
 
-# The sum of a list of numbers is an unchanging (deterministic)
-# quantity which may take a long time to compute. To save on
-# computation time, we cache the result of the summation after
-# the first request. The following SO answer provides a bit of
-# context for the `lru_cache` decorator which we use to cache
-# the result in memory.
-# https://stackoverflow.com/a/14731729
 @functools.lru_cache(maxsize=None)
 def memoized_summation():
+    ''' Computes the total amount (which is the core of the API response).
+    
+    The value is cached in memory after the first invocation.
+    '''
+    # The sum of a list of numbers is an unchanging (deterministic)
+    # quantity which may take a long time to compute. To save on
+    # computation time, we cache the result of the summation after
+    # the first request. The following SO answer provides a bit of
+    # context for the `lru_cache` decorator which we use to cache
+    # the result in memory.
+    # https://stackoverflow.com/a/14731729
     return sum_numbers_to_add()
 
 @route('/total')
