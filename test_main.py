3c7ce61f5d6236b2225f4556fa53dc4f1abd1e22
Andrei D. Robu <andreidrobu@gmail.com>
2020-09-26 19:33:01 +0100

    test sum_numbers_to_add function

--- test_main.py
+++ test_main.py
@@ -1,13 +1,17 @@
 
 import unittest.mock
 from unittest.mock import MagicMock
-from main import memoized_summation
+import main
+
+def test_sum_numbers_to_add():
+    ''' test that it actually sums up numbers '''
+    assert main.sum_numbers_to_add([1, 2, 3]) == 6
 
 def test_memoized_summation_once_only():
     ''' test that memoized_summation() calls expensive function only once '''
     mock_internal = MagicMock()
     with unittest.mock.patch('main.sum_numbers_to_add', mock_internal):
-        memoized_summation()
-        memoized_summation()
-        memoized_summation()
+        main.memoized_summation()
+        main.memoized_summation()
+        main.memoized_summation()
         mock_internal.assert_called_once()
